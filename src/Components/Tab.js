79796dd6e80be42ea0aa8ad2863c59f788931f8d
ByeongUk-An <quddnr005@naver.com>
2020-09-02 20:08:15 +0900

    Tab 수정

--- src/Components/Tab.js
+++ src/Components/Tab.js
@@ -1,5 +1,5 @@
-import React, {useRef} from 'react';
-import styled from 'styled-components';
+import React, { useRef } from "react";
+import styled from "styled-components";
 
 const BtnWrap = styled.div`
   display: flex;
@@ -10,19 +10,20 @@ const BtnWrap = styled.div`
   height: 31px;
   overflow: hidden;
   position: relative;
+  /* 포지션으로 어떻게 하지... */
 `;
 const Btn = styled.div`
   display: flex;
   height: 100%;
   align-items: center;
-  color:#032541;
+  color: #032541;
   font-weight: 600;
   font-size: 16px;
   cursor: pointer;
   border-radius: 50px;
-  
+
   & > h3 {
-  display: inline-flex;
+    display: inline-flex;
     align-content: center;
     align-items: center;
     justify-content: center;
@@ -32,43 +33,48 @@ const Btn = styled.div`
     white-space: nowrap;
   }
   &:first-child > h3 {
-  color: #fff;
+    color: #fff;
   }
   .slide-bg {
-  position: absolute;
-  left: 0;
-  z-index: -1;
-  width: 95.38px;
-  height: 100%;
-  background: #052541;
-  border-radius: 50px;
+    position: absolute;
+    left: 0;
+    z-index: -1;
+    width: 95.38px;
+    height: 100%;
+    background: #052541;
+    border-radius: 50px;
   }
 `;
 
-
 function Tab() {
-    const tabRef = useRef();
-    const tabBtns = useRef();
+  const tabRef = useRef();
+  const tabBtns = useRef();
 
-    const tabSelect = () => {
-        tabRef.current.style.left = "95.38px";
-        //tabRef.current.style.width = tabBtns.current.style.width;
-        console.log(tabBtns.current.style)
-    }
+  const tabSelect = () => {
+    tabRef.current.style.left = "95.38px";
+    //tabRef.current.style.width = tabBtns.current.style.width;
+    console.log(tabBtns.current.style);
+  };
 
-    return(
-        <>
-        <BtnWrap>
-            <Btn>
-                <h3>스트리밍</h3>
-                <div ref={tabRef} className="slide-bg"></div>
-            </Btn>
-            <Btn ref={tabBtns} onClick={tabSelect}><h3>TV</h3></Btn>
-            <Btn><h3>대여</h3></Btn>
-            <Btn><h3>극장</h3></Btn>
-        </BtnWrap>
-        </>
-    )
+  return (
+    <>
+      <BtnWrap>
+        <Btn>
+          <h3>스트리밍</h3>
+          <div ref={tabRef} className="slide-bg"></div>
+        </Btn>
+        <Btn ref={tabBtns} onClick={tabSelect}>
+          <h3>TV</h3>
+        </Btn>
+        <Btn>
+          <h3>대여</h3>
+        </Btn>
+        <Btn>
+          <h3>극장</h3>
+        </Btn>
+      </BtnWrap>
+    </>
+  );
 }
 
 export default Tab;
